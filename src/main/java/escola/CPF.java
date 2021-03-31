add452b01cd388f2df6fdb52709f17fce0cab79f
Jean Semolini <jean.maiko@totvs.com.br>
2021-03-30 23:21:53 -0300

    Adicionando telefones

---
+++ src/main/java/escola/CPF.java
@@ -0,0 +1,18 @@
+package escola;
+
+public class CPF {
+
+    private String numero;
+
+    public CPF(String numero) {
+        if (!numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("CPF inválido!");
+        }
+
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+}
