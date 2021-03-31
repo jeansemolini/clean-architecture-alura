8bed35184a3e308a31dfaa0f4bb8d63aa13e2121
Jean Semolini <jean.maiko@totvs.com.br>
2021-03-30 23:38:54 -0300

    Classe de indicações

--- src/main/java/escola/Aluno.java
+++ src/main/java/escola/Aluno.java
@@ -10,8 +10,26 @@ public class Aluno {
     private Email email;
     private List<Telefone> telefones = new ArrayList<>();
 
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
     public void adicionarTelefone(String ddd, String numero) {
         telefones.add(new Telefone(ddd, numero));
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Email getEmail() {
+        return email;
     }
 
 }
