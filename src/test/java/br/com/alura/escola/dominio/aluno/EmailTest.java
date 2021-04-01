317e8227d5594c9a73b82ddd65a3545e658ba8b3
Jean Semolini <jean.maiko@totvs.com.br>
2021-04-01 13:03:33 -0300

    Isolando a camada de dominio

--- src/test/java/br/com/alura/escola/dominio/aluno/EmailTest.java
+++ src/test/java/br/com/alura/escola/dominio/aluno/EmailTest.java
@@ -1,6 +1,5 @@
-package br.com.alura.escola.aluno;
+package br.com.alura.escola.dominio.aluno;
 
-import br.com.alura.escola.aluno.Email;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/br/com/alura/escola/dominio/aluno/TelefoneTest.java
+++ src/test/java/br/com/alura/escola/dominio/aluno/TelefoneTest.java
@@ -1,4 +1,4 @@
-package br.com.alura.escola.aluno;
+package br.com.alura.escola.dominio.aluno;
 
 import org.junit.jupiter.api.Test;
 
